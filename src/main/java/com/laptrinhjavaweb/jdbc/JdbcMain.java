4cbfb5e3869a63b6182ee18f6598ccdeaaf66b95
npthao110 <n.p.thao.110@gmail.com>
2024-04-14 23:46:35 -0500

    init project

---
+++ src/main/java/com/laptrinhjavaweb/jdbc/JdbcMain.java
@@ -0,0 +1,33 @@
+package com.laptrinhjavaweb.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcMain {
+	static final String DB_URL = "jdbc:mysql://localhost:3306/javacore42024";
+	   static final String USER = "root";
+	   static final String PASS = "Caonguyenk1@";
+	   static final String QUERY = "SELECT * FROM building";
+
+	   public static void main(String[] args) {
+	      // Open a connection
+	      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	         Statement stmt = conn.createStatement();
+	         ResultSet rs = stmt.executeQuery(QUERY);
+	      ) {		      
+	         while(rs.next()){
+	            //Display values
+	            System.out.print("Name: " + rs.getString("name"));
+	            System.out.print(", Street: " + rs.getString("street"));
+	            System.out.print(", District: " + rs.getString("district"));
+	            System.out.print(", Ward: " + rs.getString("ward"));
+	            System.out.println(", FloorArea: " + rs.getString("floorarea"));
+	         }
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      } 
+	   }
+}
