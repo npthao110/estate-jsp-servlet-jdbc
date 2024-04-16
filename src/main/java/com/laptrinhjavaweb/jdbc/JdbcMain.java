addf5ae5d8a814d5d4473431f59c09035497efe9
npthao110 <n.p.thao.110@gmail.com>
2024-04-15 20:29:16 -0500

    beginner code

--- src/main/java/com/laptrinhjavaweb/jdbc/JdbcMain.java
+++ src/main/java/com/laptrinhjavaweb/jdbc/JdbcMain.java
@@ -3,6 +3,7 @@ package com.laptrinhjavaweb.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,8 +14,8 @@ public class JdbcMain {
 	   static final String QUERY = "SELECT * FROM building";
 
 	   public static void main(String[] args) {
-	      // Open a connection
-	      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		   //experienced code
+		   /*try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 	         Statement stmt = conn.createStatement();
 	         ResultSet rs = stmt.executeQuery(QUERY);
 	      ) {		      
@@ -28,6 +29,27 @@ public class JdbcMain {
 	         }
 	      } catch (SQLException e) {
 	         e.printStackTrace();
-	      } 
+	      }*/
+		   
+		   // beginner code
+		   try {
+			   Class.forName("com.mysql.jdbc.Driver");
+			   Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		       Statement stmt = conn.createStatement();
+		       ResultSet rs = stmt.executeQuery(QUERY);
+		       while(rs.next()){
+		            System.out.print("Name: " + rs.getString("name"));
+		            System.out.print(", Street: " + rs.getString("street"));
+		            System.out.print(", District: " + rs.getString("district"));
+		            System.out.print(", Ward: " + rs.getString("ward"));
+		            System.out.println(", FloorArea: " + rs.getString("floorarea"));
+		       }
+		       conn.close();
+		       stmt.close();
+		       rs.close();
+		   } catch (ClassNotFoundException | SQLException e) {
+			   System.out.println("Error: " + e.getMessage());
+		   }
+		   
 	   }
 }
