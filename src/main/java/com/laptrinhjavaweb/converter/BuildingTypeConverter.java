15df0f1666e3b20da1d8cec95dfedbefbfe84b24
npthao110 <n.p.thao.110@gmail.com>
2024-05-15 11:25:49 -0500

    errors in BuildingTypeConverter

--- src/main/java/com/laptrinhjavaweb/converter/BuildingTypeConverter.java
+++ src/main/java/com/laptrinhjavaweb/converter/BuildingTypeConverter.java
@@ -3,20 +3,34 @@ package com.laptrinhjavaweb.converter;
 import java.util.*;
 
 import com.laptrinhjavaweb.enums.BuildingTypeEnums;
+import com.laptrinhjavaweb.utils.StringUtils;
 
 public class BuildingTypeConverter {
 
 	public static String convertType(String oldType) {
 		List<String> newTypes = new ArrayList<>();
 	    if(oldType != null) { 
-	        for(String item: oldType.split(",")) {
-	            for (BuildingTypeEnums value : BuildingTypeEnums.values()) {
-	                if (value.name().equalsIgnoreCase(item)) {
-	                    newTypes.add(value.getValue());
-	                }
-	            }
+//	        for(String item: oldType.split(",")) {
+//	            for (BuildingTypeEnums value : BuildingTypeEnums.values()) {
+//	                if (value.name().equalsIgnoreCase(item)) {
+//	                    newTypes.add(value.getValue());
+//	                }
+//	            }
+//	        }
+	        
+	        StringBuilder convertedType = new StringBuilder();
+	        
+	        String[] typeArray = oldType.split(",");
+
+	        for (String item : typeArray) {
+	        	convertedType.append("\n- " + BuildingTypeEnums.valueOf(item).getValue());
 	        }
-	        return String.join(", ", newTypes);
+	        
+//	        buildingSearchOutput.setType(convertedType.toString());
+	        
+	        return String.join(", ", convertedType.toString());
+
+//	        return String.join(", ", newTypes);
 	    }
 	    return null;
     }
--- src/main/java/com/laptrinhjavaweb/enums/BuildingTypeEnums.java
+++ src/main/java/com/laptrinhjavaweb/enums/BuildingTypeEnums.java
@@ -1,18 +1,34 @@
-package com.laptrinhjavaweb.enums;
+//package com.laptrinhjavaweb.enums;
+//
+//public enum BuildingTypeEnums {
+//
+//    tang_tret ("tầng trệt"),  
+//    nguyen_can ("nguyên căn"),  
+//    noi_that ("nội thất");
+//	
+//	private final String value;
+//
+//    private BuildingTypeEnums(String value) {
+//        this.value = value;
+//    }
+//
+//    public String getValue() {
+//		return value;
+//	}
+//}
 
 public enum BuildingTypeEnums {
+    TANG_TRET("Tầng trệt"),
+    NGUYEN_CAN("Nguyeen căn"),
+    NOI_THAT("Nội thất");
 
-    tang_tret ("tầng trệt"),  
-    nguyen_can ("nguyên căn"),  
-    noi_that ("nội thất");
-	
-	private final String value;
+    private final String value;
 
-    private BuildingTypeEnums(String value) {
+    BuildingTypeEnums(String value) {
         this.value = value;
     }
 
     public String getValue() {
-		return value;
-	}
+        return value;
+    }
 }
